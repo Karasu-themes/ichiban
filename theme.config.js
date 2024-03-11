a4a9be565bd1488bb7edfcc395515e667de9dde4
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-03-11 16:51:39 -0300

    Se ha actualizado a la licencia correcta

--- theme.config.js
+++ theme.config.js
@@ -8,7 +8,7 @@ export default {
     description: "A basic blogger template created with BloggerBase",
     version: "1.0.0",
     url: "https://github.com/Karasu-themes/BloggerBase",
-    license: "Release under MIT License",
+    license: "Release under GPL-v3.0 License",
     author: {
       name: "MarceloTLD",
       url: "https://github.com/MarceloTLD"
